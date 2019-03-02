bb3fc2abf33b9c751ef106aad8fe68f96a7df7e8
Adam Urbina <adamurbina@gmail.com>
2019-03-02 13:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function theTruth() {
   return true;
 }
 
+function sayHey(){
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
 
 function favoriteIceCream(flavor){
   debugger;
